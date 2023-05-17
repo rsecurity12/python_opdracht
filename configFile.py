f0e29c2d6787248cea6041166e8d1445b34d97c1
rsecurity12 <94355752+rsecurity12@users.noreply.github.com>
2023-05-17 13:48:32 +0200

    Add files via upload

---
+++ configFile.py
@@ -0,0 +1,10 @@
+class ConfigFile:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def instruction(script):
+        # Add your instructions here
+        print(f"{script}")
+
+ConfigFile.instruction("screenshot.py")
