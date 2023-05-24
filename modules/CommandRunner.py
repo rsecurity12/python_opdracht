04fb6fed664fdbd64d35cbd1bb8fbe7e7b3f76f7
Rodrigo <rodrigo.caldeirasilva@student.ap.be>
2023-05-24 15:57:14 +0200

    Update modules

--- modules/CommandRunner.py
+++ modules/CommandRunner.py
@@ -1,11 +1,11 @@
 import subprocess
 import os
 
-class CommandExecutioner():
+class CommandRunner():
   def __init__(self, script_name):
     self.script_name = script_name
     
-  def Run_CommandExecutioner(self,command="find / -perm -g=s -o -perm -4000 ! -type l -maxdepth 3 -exec ls -ld {} \; 2>/dev/null", *args):
+  def Run_CommandRunner(self,command="find / -perm -g=s -o -perm -4000 ! -type l -maxdepth 3 -exec ls -ld {} \; 2>/dev/null", *args):
     full_command = "{} {}".format(command, ' '.join(args))
     result = subprocess.run(full_command, shell=True, capture_output=True, text=True)
     output = result.stdout.strip()
@@ -17,5 +17,5 @@ class CommandExecutioner():
     with open(os.path.join(destination_directory, "result.txt"), "w") as f:
         f.write(output)
  
-commandExecutioner = CommandExecutioner("CommandExecutioner")
-commandExecutioner.Run_CommandExecutioner()
+commandExecutioner = CommandRunner("CommandExecutioner")
+commandExecutioner.Run_CommandRunner()
